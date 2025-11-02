af24880c0e8f565142a7c7731abbb5bed273251d
Calebe Souza <swplayermc30@gmail.com>
2025-11-02 11:20:26 -0300

    Added everything

---
+++ lua/config/options.lua
@@ -0,0 +1,3 @@
+vim.opt.shiftwidth = 4
+vim.opt.cursorline = true
+vim.opt.number = true
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,23 @@
+return {
+
+    "nvim-treesitter/nvim-treesitter", branch = "master",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+	local configs = require("nvim-treesitter.configs")
+	configs.setup({
+	    highlight = {
+		enable = true,
+	    },
+	    indent = { enable = true },
+	    autotage = { enable = true },
+	    ensure_installed = {
+		"lua",
+		"c_sharp",
+		"python",
+		"bash"
+	    },
+	    auto_install = false
+	})
+    end
+}
