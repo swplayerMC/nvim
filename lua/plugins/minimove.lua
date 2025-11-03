90af8d9348c18a9fef79d344e9ba45d3052a1c8d
Calebe Souza <swplayermc30@gmail.com>
2025-11-03 13:11:53 -0300

    Trying to fix mini.move config

--- lua/plugins/minimove.lua
+++ lua/plugins/minimove.lua
@@ -1,23 +1,25 @@
 return {
     "nvim-mini/mini.nvim", version = false,
+    config = function()
+	require("mini.move").setup({
+            -- Module mappings. Use `''` (empty string) to disable one.
+	    mappings = {
+	       -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+		left = 'H',
+		right = 'L',
+		down = 'J',
+		up = 'K',
 
-    -- Module mappings. Use `''` (empty string) to disable one.
-    mappings = {
-	-- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
-        left = '<M-h>',
-        right = '<M-l>',
-        down = '<M-j>',
-        up = '<M-k>',
-
-	-- Move current line in Normal mode
-        line_left = '<M-h>',
-        line_right = '<M-l>',
-        line_down = '<M-j>',
-        line_up = '<M-k>',
-    },
-    -- Options which control moving behavior
-    options = {
-	-- Automatically reindent selection during linewise vertical move
-        reindent_linewise = true,
-    },
+     	    -- Move current line in Normal mode
+            line_left = '<A-h>',
+            line_right = '<A-l>',
+            line_down = '<A-j>',
+            line_up = '<A-k>',
+        },
+	-- Options which control moving behavior
+        options = {
+	    -- Automatically reindent selection during linewise vertical move
+	    reindent_linewise = true,
+	}})
+    end
 }
