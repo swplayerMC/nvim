7087476f3d8a4c781be445768dbfb53c058609bf
Calebe <swplayermc30@gmail.com>
2025-11-04 17:38:44 -0300

    Started configuring 'todo-comments'

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -2,3 +2,15 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>e", "<cmd>Neotree<CR>")
+
+vim.keymap.set("n", "[t", 
+  function()
+    require("todo-comments").jump_prev() 
+  end, { desc = "Previous todo comment" })
+
+
+vim.keymap.set("n", "]t", 
+  function()
+    require("todo-comments").jump_next() 
+  end, { desc = "Next todo comment" })
+
