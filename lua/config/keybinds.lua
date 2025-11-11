179303ff763eb99812a3ff370bcd1691df1abd12
Calebe <swplayermc30@gmail.com>
2025-11-11 10:52:28 -0300

    Cleaning some white spaces

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -4,14 +4,10 @@ vim.g.clipboard = "xclip"
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 vim.keymap.set("n", "<C-e>", "<cmd>Neotree<CR>")
 
-vim.keymap.set("n", "[t", 
-  function()
+vim.keymap.set("n", "[t", function()
     require("todo-comments").jump_prev() 
   end, { desc = "Previous todo comment" })
 
-
-vim.keymap.set("n", "]t", 
-  function()
-    require("todo-comments").jump_next() 
+vim.keymap.set("n", "]t", function()
+    require("todo-comments").jump_next()
   end, { desc = "Next todo comment" })
-
