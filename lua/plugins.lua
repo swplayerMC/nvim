673bc4e88d8706af74eedd3ad49271c666283457
Calebe <swplayermc30@gmail.com>
2025-11-11 10:41:34 -0300

    Snnipets! Let's go

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -16,15 +16,22 @@ return {
     }
   },
   {
-    "mason-org/mason.nvim",
-    opts = {
-      ui = {
-	icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗"
-	}
-      }
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+      {
+	"mason-org/mason.nvim",
+	opts = {
+	  ui = {
+	    icons = {
+	      package_installed = "✓",
+	      package_pending = "➜",
+	      package_uninstalled = "✗"
+	    }
+	  }
+	},
+      },
+      "neovim/nvim-lspconfig"
     }
   },
   -- // Telescope, Harpoon and Fuzzy Finds // --
@@ -132,6 +139,7 @@ return {
   -- // Keymap and typing related plugins // --
   {
     "hrsh7th/nvim-cmp",
+    event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
       "neovim/nvim-lspconfig",
       "hrsh7th/cmp-nvim-lsp",
@@ -147,11 +155,13 @@ return {
     },
     config = function()
       local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
       cmp.setup({
 	snippet = {
 	  expand = function(args)
-	    require("luasnip").lsp_expand(args.body)
-	    vim.snippet.expand(args.body)
+	    luasnip.lsp_expand(args.body)
+	    -- vim.snippet.expand(args.body)
 	  end
 	},
 	window = {
@@ -163,13 +173,52 @@ return {
 	  ["<C-f>"] = cmp.mapping.scroll_docs(4),
 	  ["<C-Space>"] = cmp.mapping.complete(),
 	  ["<C-e>"] = cmp.mapping.abort(),
-	  ["<CR>"] = cmp.mapping.confirm({ select = true }),
+	  ["<CR>"] = cmp.mapping.confirm({ select = false }),
+	  ["<Tab>"] = cmp.mapping(function(fallback)
+	    -- Prefer jumping if both jumping and expanding are available
+	    -- Otherwise, you may recursively expand a snippet without ever jumping
+	    -- (which is annoying)
+	    if cmp.visible() then
+	      cmp.select_next_item()
+	    elseif luasnip.jumpable(1) then
+	      luasnip.jump(1)
+	    elseif luasnip.expandable() then
+	      luasnip.expand()
+	    else
+	      fallback()
+	    end
+	  end, { "i", "s" }),
+	  ["<S-Tab>"] = cmp.mapping(function(fallback)
+	    -- Prefer jumping if both jumping and expanding are available
+	    -- Otherwise, you may recursively expand a snippet without ever jumping
+	    -- (which is annoying)
+	    if cmp.visible() then
+	      cmp.select_prev_item()
+	    elseif luasnip.jumpable(-1) then
+	      luasnip.jump(-1)
+	    else
+	      fallback()
+	    end
+	  end, { "i", "s" }),
 	}),
 	sources = cmp.config.sources({
 	  { name = "nvim_lsp" },
 	  { name = "luasnip" },
 	}, {
-	  { name = "buffer" },
+	  {
+	    name = "buffer",
+	    option = {
+	      keyword_pattern = [[\k\+]],
+	      -- Enable completion from all visible buffers
+	      get_bufnrs = function()
+		local bufs = {}
+		for _, win in ipairs(vim.api.nvim_list_wins()) do
+		  bufs[vim.api.nvim_win_get_buf(win)] = true
+		end
+		return vim.tbl_keys(bufs)
+	      end,
+	    },
+	  }
 	})
       })
       cmp.setup.cmdline(":", {
